5927a871b8d0bf3cb6ca0f98f825dae4ce06817c
AdrienBLANCHART <adrien.blanchart@philibertnet.com>
2024-07-23 10:29:16 +0200

    removed type anotation

--- pibooth/camera/pibooth_picamera2.py
+++ pibooth/camera/pibooth_picamera2.py
@@ -102,7 +102,8 @@ class Rpi_Picamera2(RpiCamera):
         else:
             return self.resolution
     
-    def _rotate_image(self, image:PIL.Image.Image | pygame.Surface):
+    #def _rotate_image(self, image:PIL.Image.Image | pygame.Surface):
+    def _rotate_image(self, image):
         """Rotate image clockwise"""
         # Camera rotation is the same for both preview and capture
         if self.capture_rotation != 0 and self.preview_rotation != 0:
