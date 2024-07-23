555f7f796f978b0aa356521eb5a594fcf5502e8e
AdrienBLANCHART <adrien.blanchart@philibertnet.com>
2024-07-23 10:39:29 +0200

    test with v2 cam plugin

--- pibooth/plugins/camera_plugin.py
+++ pibooth/plugins/camera_plugin.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import time
-import math
+import pygame
 import pibooth
-from pibooth import evts
 from pibooth import camera
-from pibooth.language import get_translated_text
-from pibooth.utils import LOGGER, PollingTimer
+from pibooth.utils import LOGGER
 
 
-class CameraPlugin:
+class CameraPlugin(object):
 
     """Plugin to manage the camera captures.
     """
 
-    __name__ = 'pibooth-core:camera'
+    name = 'pibooth-core:camera'
 
     def __init__(self, plugin_manager):
         self._pm = plugin_manager
-        self.timer = PollingTimer()
-        self.capture_count = 0
+        self.count = 0
 
     @pibooth.hookimpl(hookwrapper=True)
     def pibooth_setup_camera(self, cfg):
@@ -27,7 +24,7 @@ class CameraPlugin:
         cam = outcome.get_result()
 
         if not cam:
-            LOGGER.debug("Use pibooth camera management system")
+            LOGGER.debug("Fallback to pibooth default camera management system")
             cam = camera.find_camera()
 
         cam.initialize(cfg.gettuple('CAMERA', 'iso', (int, str), 2),
@@ -35,7 +32,6 @@ class CameraPlugin:
                        cfg.gettuple('CAMERA', 'rotation', int, 2),
                        cfg.getboolean('CAMERA', 'flip'),
                        cfg.getboolean('CAMERA', 'delete_internal_memory'))
-
         outcome.force_result(cam)
 
     @pibooth.hookimpl
@@ -59,52 +55,62 @@ class CameraPlugin:
             app.capture_nbr = app.capture_choices[0]
 
     @pibooth.hookimpl
+    def state_choose_do(self, app, events):
+        event = app.find_choice_event(events)
+        if event:
+            if event.key == pygame.K_LEFT:
+                app.capture_nbr = app.capture_choices[0]
+            elif event.key == pygame.K_RIGHT:
+                app.capture_nbr = app.capture_choices[1]
+
+    @pibooth.hookimpl
     def state_preview_enter(self, cfg, app, win):
         LOGGER.info("Show preview before next capture")
-        border = 100
+        if not app.capture_date:
+            app.capture_date = time.strftime("%Y-%m-%d-%H-%M-%S")
         app.camera.preview(win)
-        self.timer.start(cfg.getint('WINDOW', 'preview_delay'))
 
     @pibooth.hookimpl
     def state_preview_do(self, cfg, app):
+        pygame.event.pump()  # Before blocking actions
         if cfg.getboolean('WINDOW', 'preview_countdown'):
-            if self.timer.remaining() > 0.5:
-                app.camera.set_overlay(math.ceil(self.timer.remaining()))
-        if self.timer.remaining() <= 0.5:
-            app.camera.set_overlay(get_translated_text('smile'))
+            app.camera.preview_countdown(cfg.getint('WINDOW', 'preview_delay'))
+        else:
+            app.camera.preview_wait(cfg.getint('WINDOW', 'preview_delay'))
 
     @pibooth.hookimpl
-    def state_preview_validate(self):
-        if self.timer.is_timeout():
-            return 'capture'
+    def state_preview_exit(self, cfg, app):
+        if cfg.getboolean('WINDOW', 'preview_stop_on_capture'):
+            app.camera.stop_preview()
 
     @pibooth.hookimpl
-    def state_capture_enter(self, cfg, app):
+    def state_capture_do(self, cfg, app, win):
         effects = cfg.gettyped('PICTURE', 'captures_effects')
-        if not app.capture_date:
-            app.capture_date = time.strftime("%Y-%m-%d-%H-%M-%S")
-
         if not isinstance(effects, (list, tuple)):
             # Same effect for all captures
             effect = effects
         elif len(effects) >= app.capture_nbr:
             # Take the effect corresponding to the current capture
-            effect = effects[self.capture_count]
+            effect = effects[self.count]
         else:
             # Not possible
-            raise ValueError(f"Not enough effects defined for {app.capture_nbr} captures {effects}")
+            raise ValueError("Not enough effects defined for {} captures {}".format(
+                app.capture_nbr, effects))
 
         LOGGER.info("Take a capture")
-        app.camera.capture(effect)
+        if cfg.getboolean('WINDOW', 'flash'):
+            with win.flash(2):  # Manage the window here, have no choice
+                app.camera.capture(effect)
+        else:
+            app.camera.capture(effect)
+
+        self.count += 1
 
     @pibooth.hookimpl
-    def state_capture_do(self, events):
-        event = evts.find_event(events, evts.EVT_PIBOOTH_CAM_CAPTURE)
-        if event:
-            self.capture_count += 1
+    def state_capture_exit(self, cfg, app):
+        if not cfg.getboolean('WINDOW', 'preview_stop_on_capture'):
+            app.camera.stop_preview()
 
     @pibooth.hookimpl
     def state_processing_enter(self, app):
-        self.capture_count = 0
-        LOGGER.info("Stopping preview")
-        app.camera.stop_preview()
+        self.count = 0
